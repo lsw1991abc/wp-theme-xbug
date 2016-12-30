8e7e6e46945dc7f2098771e985a6f81653e2fc9d
Wizard <lsw1991abc@gmail.com>
2016-12-30 11:34:53 +0800

    初步完成文章单页的数据展示

--- template/content-single.php
+++ template/content-single.php
@@ -5,28 +5,49 @@
 ?>
 <div id="post-<?php the_ID() ?>" class="xbug-article-block">
   <div class="mdl-typography--display-1">
-    <?php the_title(); ?>
+      <?php the_title(); ?>
   </div>
-  <div class="mdl-typography--font-light">标签：tag 时间：2016-12-12 作者：Wizard</div>
-  <hr/>
+  <div class="mdl-typography--font-light"><?php the_tags() ?> 时间：<?php echo get_the_date() . ' ' .
+          get_the_time(); ?>作者：<?php the_author(); ?></div>
+  <hr />
   <div class="xbug-article-content">
-    <?php the_content(); ?>
+      <?php the_content(); ?>
   </div>
-  <hr/>
+  <hr />
   <div class="mdl-typography--subhead-color-contrast">
-    本文链接：<a href="<?php echo get_the_permalink(); ?>" title="<?php the_title()?>"><?php echo get_the_permalink(); ?></a>
+    本文链接：<a href="<?php echo get_the_permalink(); ?>" title="<?php the_title() ?>"><?php echo get_the_permalink(); ?></a>
   </div>
-  <hr/>
+  <hr />
   <div class="mdl-typography--subhead-color-contrast">
-    <?php
-    // 相关文章
-    ?>
+    <p>相关文章</p>
+      <?php
+      // 相关文章
+      $postTags = get_the_tags();
+      if ($postTags) {
+          $tags = '';
+          foreach ($postTags as $postTag) {
+              $tags .= $postTag->term_id . ',';
+          }
+          query_posts(array(
+              'tag__in' => explode(',', $tags),
+              'post__not_in' => explode(',', $post->ID)
+          ));
+          while (have_posts()) {
+              the_post();
+              ?>
+            <a href="<?php the_permalink(); ?>" target="_blank"><?php the_title(); ?></a>
+              <?php
+          }
+          wp_reset_query();
+      }
+      ?>
   </div>
-  <hr/>
+  <hr />
   <div class="mdl-typography--subhead-color-contrast">
-    评论区
-  </div>
-  <div class="mdl-typography--subhead-color-contrast">
-    历史评价
+      <?php
+      if (comments_open() || get_comments_number()) :
+          comments_template();
+      endif;
+      ?>
   </div>
 </div>
