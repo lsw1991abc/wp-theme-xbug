dc6f8a99144ac8108119b3fdc026e84b65b1726c
Wizard <lsw1991abc@gmail.com>
2017-01-10 16:34:38 +0800

    修改侧边栏样式展示

--- sidebar.php
+++ sidebar.php
@@ -3,18 +3,18 @@
  * 边栏模块
  */
 ?>
-<div class="mdl-cell mdl-cell--3-col xbug-card-list xbug-sidebar">
-  <div class="mdl-cell mdl-cell--12-col mdl-card">
-    <div class="mdl-card__actions">
-      <a href="#"><i class="fa fa-wechat fa-2x mdl-color-text--green"></i></a>
-      <a href="#"><i class="fa fa-qq fa-2x mdl-color-text--indigo-900"></i></a>
-      <a href="#"><i class="fa fa-weibo fa-2x mdl-color-text--deep-orange-500"></i></a>
-      <a href="#"><i class="fa fa-google-plus-circle fa-2x mdl-color-text--red"></i></a>
-    </div>
-  </div>
+<ul class="mdl-cell mdl-cell--3-col xbug-card-list xbug-sidebar">
+  <li class="widget">
+    <ul class="mdl-card__actions">
+      <li><a href="#"><i class="fa fa-wechat fa-2x mdl-color-text--green"></i></a></li>
+      <li><a href="#"><i class="fa fa-qq fa-2x mdl-color-text--indigo-900"></i></a></li>
+      <li><a href="#"><i class="fa fa-weibo fa-2x mdl-color-text--deep-orange-500"></i></a></li>
+      <li><a href="#"><i class="fa fa-google-plus-circle fa-2x mdl-color-text--red"></i></a></li>
+    </ul>
+  </li>
   <?php
   if (is_active_sidebar('home-sidebar')) {
     dynamic_sidebar('home-sidebar');
   }
   ?>
-</div>
+</ul>
