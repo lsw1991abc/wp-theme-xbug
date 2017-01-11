665be1036a6f64ea83bd152290d52b03e9b937a0
Wizard <lsw1991abc@gmail.com>
2017-01-11 11:40:42 +0800

    修改文章归档页展示

--- template/page-archive.php
+++ template/page-archive.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: 文章归档
+ * Template Name: 文章归档2
  */
 get_header();
 ?>
@@ -9,42 +9,45 @@ get_header();
   <div class="mdl-cell mdl-cell--10-col">
     <div class="xbug-article-block">
       <div class="mdl-typography--display-1"><?php the_title(); ?></div>
-      <hr/>
+      <hr />
       <div class="xbug-article-content">
         <?php
-        $xbugPosts = get_posts('numberposts=-1&orderby=post_date&order=DESC');
-
-        $pre_year = $year = 0;
-        $pre_month = $month = 0;
-        $pre_day = $day = 0;
-        foreach ($xbugPosts as $post) {
-          setup_postdata($post);
-          $year = get_the_date('Y');
-          $month = get_the_date('F');
-          $day = get_the_date('j');
-          if ($pre_year != $year || $pre_month != $month) {
-            echo '<ul><p>' . $year . ' ' . $month . '</p>';
+        $xbugPosts = get_posts( 'numberposts=-1&orderby=post_date&order=DESC' );
+        if ( count( $xbugPosts ) > 0 ) {
+          $prev_year  = $year = 0;
+          $prev_month = $month = 0;
+          $not_first  = false;
+          foreach ( $xbugPosts as $post ) {
+            setup_postdata( $post );
+            $year  = get_the_date( 'Y' );
+            $month = get_the_date( 'n' );
+            $day   = get_the_date( 'j' );
+            if ( $year != $prev_year || $month != $prev_month ) {
+              if ( $not_first ) {
+                echo '</ul></div>';
+              }
+              echo '<div><h3>' . get_the_date( 'F Y' ) . '</h3>' . '<ul>';
+              $not_first = true;
+            }
+            $prev_year  = $year;
+            $prev_month = $month;
+            ?>
+            <li>
+              <time><?php echo $day; ?>日</time>
+              <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+              <span class="muted"><?php comments_number( '', '1评论', '%评论' ); ?></span>
+            </li>
+            <?php
           }
-
-          ?>
-          <li>
-            <time><?php echo $day; ?>日</time>
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            <span><?php comments_number('', '1评论', '%评论'); ?></span>
-          </li>
-          <?php
-          if ($pre_year != $year || $pre_month != $month) {
-            echo '</ul>';
-          }
-          $pre_year = $year;
-          $pre_month = $month;
+          echo '</ul></div>';
         }
         ?>
+
       </div>
-      <hr/>
+      <hr />
       <div class="mdl-typography--subhead-color-contrast">
         <?php
-        if (comments_open() || get_comments_number()) {
+        if ( comments_open() || get_comments_number() ) {
           comments_template();
         }
         ?>
