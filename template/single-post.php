a856ab4dc6c601c029d417e489ca789698bf4df0
Wizard <lsw1991abc@gmail.com>
2017-01-15 03:26:55 +0800

    文章列表加入新窗口打开按钮

--- template/single-post.php
+++ template/single-post.php
@@ -7,12 +7,17 @@ $templateDirUri = esc_url(get_template_directory_uri());
 <div id="post-<?php the_ID(); ?>" class="mdl-cell mdl-cell--12-col mdl-card">
   <div class="mdl-card__title">
     <h4 class="mdl-card__title-text">
-      <!--<label class="xbug-catalog__label"><a href="#">文章标题</a> <s></s></label>-->
-      <?php the_title(sprintf('<a href="%s" target="_blank">', esc_url(get_permalink())), '</a>'); ?>
+      <?php
+      $current_permalink = esc_url(get_permalink());
+      the_title(sprintf('<a href="%s">', $current_permalink), '</a>&nbsp;&nbsp;
+        <a href="' . $current_permalink . '" target="_blank" title="新窗口打开">
+          <i class="fa fa-external-link"></i>
+        </a>');
+      ?>
     </h4>
   </div>
   <div class="xbug-card__rich-media">
-    <div class="mdl-card__media xbug-media__width-max">
+    <div class="mdl-card__media">
       <a href="<?php the_permalink(); ?>" target="_blank">
         <img src="<?php echo xbug_get_thumbnail_src(); ?>" alt="<?php the_title(); ?>"
              title="<?php the_title(); ?>"
@@ -24,7 +29,7 @@ $templateDirUri = esc_url(get_template_directory_uri());
       </p>
       <p class="mdl-typography--text-right">
         <i class="fa fa-clock-o fa-lg"></i> <?php the_date(); ?> &nbsp;&nbsp;&nbsp;
-        <i class="fa fa-user fa-lg"></i> <?php the_author(); ?> &nbsp;&nbsp;&nbsp;
+        <i class="fa fa-commenting-o fa-lg"></i> <?php comments_number(); ?> &nbsp;&nbsp;&nbsp;
         <a href="<?php the_permalink(); ?>" target="_blank">阅读全文</a>
       </p>
     </div>
